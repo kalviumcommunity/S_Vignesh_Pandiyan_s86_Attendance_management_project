5e6897ea69686fd268837d29d512761d7466d846
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-08-02 10:29:43 +0530

    feat: Initial project setup (Part 1)

---
+++ src/com/school/Main.java
@@ -0,0 +1,9 @@
+package com.school;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the school Attendance System Project!");
+        System.out.println(" Session 1: Project Setup and Orientation Complete.");
+    }
+    
+}
